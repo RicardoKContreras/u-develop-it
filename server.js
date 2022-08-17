830d9a0b64c6efaf8b2e6bbfaefecdbca2c242e2
Jimbei <contrerasr2272@gmail.com>
2022-08-17 12:08:08 -0400

    db.query selecting from candidates

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express Middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MYSQL username,
+        user: 'root',
+        //Your MYSQL password
+        password: 'BootCamp217!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
